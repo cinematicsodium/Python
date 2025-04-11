0be112a4a23c78d4730cf26b8fa09d71ab1595c3
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2025-04-11 15:03:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ from pathlib import Path
 import fitz
 
 from ind_processor import IndProcessor
+from utils import log
 
 
 def main():
-    folder = Path("")
+    folder: Path
 
     for file in folder.iterdir():
         if file.is_file() and file.suffix == ".pdf":
-            processor = IndProcessor(file)
-            processor.process()
+            log(file.name)
+            processor = IndProcessor()
+            data = processor.process(file)
 
 
 def testing():
     try:
-        file: Path = Path("")
+        file: Path
+        log(file.name)
         processor = IndProcessor()
-        processor.process(file)
+        data = processor.process(file)
+        log(f'\n{data}')
     except Exception as e:
-        print(f"Error: {e}")
+        log(f"[bright_red]Error: {e}[/bright_red]")
 
 
 if __name__ == "__main__":
     testing()
+
