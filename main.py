c7dc8325ed8dd99f921304572f47be328ac9dd26
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2025-04-14 15:15:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 from pathlib import Path
 
-import fitz
 from ind_processor import IndProcessor
-from utils import log
+from modules import Logger
+
+logger = Logger()
 
 
 def main():
-    folder: Path
-
-    for file in folder.iterdir():
-        if file.is_file() and file.suffix == ".pdf":
-            log(file.name)
-            processor = IndProcessor()
-            data = processor.process(file)
-
-
-def testing():
     try:
-        file: Path
-        log(file.name)
-        processor = IndProcessor()
-        data = processor.process(file)
-        log(f'\n{data}')
+        processed_list: list[str] = []
+        failed_list: list[dict[str, str]] = []
+        
+        folder: Path
+
+        for file in folder.iterdir():
+            if file.is_file() and file.suffix == ".pdf":
+                try:
+                    logger.info(file.name)
+                    processor = IndProcessor()
+                    processor.process(file)
+                    processed_list.append(file.name)
+
+                except Exception as e:
+                    logger.error(f"\n{file.name}\n{e}")
+                    failed_list.append({"file": file.name, "error": {str(e)[:100]}})
+
+        print(f"\n\nProcessed Files Count: {len(processed_list)}")
+        if processed_list:
+            [print(f"- {processed}") for processed in processed_list]
+
+        print(f"\n\nProcess Failure Count: {len(failed_list)}")
+        if failed_list:
+            [
+                print(f"- {k}: {str(v)[:100]}...")
+                for failed in failed_list
+                for k, v in failed.items()
+            ]
+        print("\n\n")
     except Exception as e:
-        log(f"[bright_red]Error: {e}[/bright_red]")
-
-
-if __name__ == "__main__":
-    testing()
-
+        print(e)
