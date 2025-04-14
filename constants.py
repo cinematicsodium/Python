7a0c627de6a9aac6b8a1b375c8b949fc1a69b3f2
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2025-04-14 15:07:31 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,11 +2,6 @@ from datetime import datetime
 from pathlib import Path
 from typing import Optional
 
-json_output_path: Path
-tsv_output_path: Path
-serial_numbers_path: Path
-logger_path: Path
-
 active_fiscal_year = 2025
 
 today = datetime.today()
@@ -22,9 +17,36 @@ if current_fiscal_year != active_fiscal_year:
         "Please update the fiscal year in constants.py."
     )
 
-monetary_matrix: tuple[tuple[int, int, int], ...]
-time_off_matrix: tuple[tuple[int, int, int], ...]
+_local_dir: Path
+_network_dir: Path
+
+json_output_path: Path = _local_dir / Path()
+tsv_output_path: Path = _local_dir / Path()
+serial_numbers_path: Path = _local_dir / Path()
+logger_path: Path = _local_dir / Path()
+archive_path: Path = _network_dir / Path()
+archive_path.mkdir(parents=True, exist_ok=True)
+
+
+class Tracker:
+    file_path: Path = _local_dir / Path()
+    sheet_name: str = "data_entry"
+    ind_coord: str = "C2"
+    grp_coord: str = "C3"
+
+
+monetary_matrix: tuple[tuple[int, int, int], ...] = (
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0),
+)
+time_off_matrix: tuple[tuple[int, int, int], ...] = (
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0),
+)
+
 
 division_map: dict[str, list[str]]
 mb_map: dict[str, list[str]]
-consultant_map: dict[str,str]
+consultant_map: dict[str, str]
