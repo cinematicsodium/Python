c5be9d5bcfef771e15676663b1906cf3dbb63ae0
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2025-04-21 12:42:23 -0600

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,31 +1,39 @@
 from datetime import datetime
 
-from constants import PathManager
+from constants import path_manager, testing_mode
 from rich.console import Console
 
 console = Console()
 
 
 class Logger:
-    def _log(self, level: str, color: str, message: str):
+    def _log(
+        self,
+        message: str,
+        level: str = "INFO",
+        color: str = "spring_green3",
+        linebreak=True,
+    ):
+        padding = "\n" if linebreak is True else ""
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-4]
 
-        console.print(f"[{color}]{now} - {level}: {message}[/{color}]")
-
-        with open(PathManager.logger_path, "a", encoding="utf-8") as f:
-            f.write(f"\n{now} - {level}: {message}")
+        console.print(
+            f"[{color}]{padding}{now} - {level}: {message}{padding}[/{color}]"
+        )
+        with open(path_manager.logger_path, "a", encoding="utf-8") as f:
+            f.write(f"\n{padding}{now} - {level}: {message}{padding}")
 
     def info(self, message):
-        self._log("INFO", "spring_green3", message)
+        self._log(message, linebreak=False)
 
     def warning(self, message):
-        self._log("WARNING", "orange1", f"\n{message}\n")
+        self._log(message, "WARNING", "orange1")
 
     def error(self, message):
-        self._log("ERROR", "red1", f"\n{message}\n")
+        self._log(message, "ERROR", "red1")
 
     def path(self, message):
-        self._log("INFO", "spring_green3", f"\n{message}")
+        self._log(message)
 
     def final(self, message):
-        self._log("INFO", "spring_green3", f"\n\n{message}\n")
+        self._log(f"\n{message}")
