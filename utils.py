f5c1020aaba093f03f316354aa269f6476d0928e
cinematicsodium <126626502+cinematicsodium@users.noreply.github.com>
2025-04-21 12:44:19 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
-from typing import Optional
+import json
 from uuid import uuid4
 
 import yaml
 from constants import (
-    PathManager,
     active_fiscal_year,
     division_map,
     mb_map,
+    path_manager,
     testing_mode,
 )
 from formatting import Formatter
@@ -18,10 +18,10 @@ class LogID:
 
     def _load(self) -> dict[str, int]:
         try:
-            if not PathManager.serial_path.exists():
-                raise ValueError(f"Log ID file not found: {PathManager.serial_path}")
+            if not path_manager.serial_path.exists():
+                raise ValueError(f"Log ID file not found: {path_manager.serial_path}")
 
-            with open(PathManager.serial_path, "r") as file:
+            with open(path_manager.serial_path, "r") as file:
 
                 log_id_data = yaml.safe_load(file)
 
@@ -55,6 +55,7 @@ class LogID:
         fy_str = str(active_fiscal_year)[-2:]
         log_serial = str(log_id_data[self.category]).zfill(3)
         log_id = f"{fy_str}-{self.category}-{log_serial}"
+        self.validate(log_id)
         return log_id
 
     def save(self) -> None:
@@ -64,30 +65,51 @@ class LogID:
         log_id_data = self._load()
         log_id_data[self.category] += 1
 
-        with open(PathManager.serial_path, "w") as file:
+        with open(path_manager.serial_path, "w") as file:
             yaml.safe_dump(log_id_data, file, indent=4, sort_keys=False)
 
+    def validate(log_id: str) -> None:
+        with open(path_manager.json_output_path, "r", encoding="utf-8") as file:
+            content: str = file.read().strip()
+            content = "[]" if not content else content
 
-def find_organization(input_org: str) -> tuple[Optional[str], Optional[str]]:
+            json_dict_list: list[dict[str, str | int | None]] = json.loads(content)
+
+        duplicates: list[dict] = [
+            json_dict_item
+            for json_dict_item in json_dict_list
+            if json_dict_item.get("log_id", None) == log_id
+        ]
+        if duplicates:
+            raise ValueError(
+                f"Duplicate entries found for Log ID {log_id}\n"
+                f"Duplicate count: {len(duplicates)}"
+            )
+
+
+def find_organization(input_org: str) -> tuple[str, str]:
     """
     Finds the organization matching the input string.
     Returns `None` if match not found.
     """
     if not input_org:
-        return
-    formatted_input = Formatter(input_org).org_div_match()
+        return ("", "")
+    formatted_input = Formatter(input_org).standardized_org_div()
 
-    org_match: Optional[str] = None
-    div_match: Optional[str] = None
+    org_match: str = ""
+    div_match: str = ""
     for target_org, div_list in division_map.items():
-        formatted_org = Formatter(target_org).org_div_match()
+        formatted_org = Formatter(target_org).standardized_org_div()
+        formatted_org = (
+            formatted_org.split("(")[0] if "(" in formatted_org else formatted_org
+        )
         div_list = list(reversed(div_list))
 
         if formatted_org in formatted_input:
             org_match = target_org
 
         for target_div in div_list:
-            formatted_div = Formatter(target_div).org_div_match()
+            formatted_div = Formatter(target_div).standardized_org_div()
 
             if formatted_div in formatted_input or formatted_input in formatted_div:
                 org_match = target_org
@@ -96,6 +118,8 @@ def find_organization(input_org: str) -> tuple[Optional[str], Optional[str]]:
         if org_match and div_match:
             break
 
+    div_match = div_match if div_match else input_org
+
     return org_match, div_match
 
 
@@ -103,16 +127,16 @@ def find_mgmt_division(input_org: str) -> str:
     if not input_org:
         return
 
-    formatted_input = Formatter(input_org).org_div_match()
+    formatted_input = Formatter(input_org).standardized_org_div()
 
     for org, div_list in mb_map.items():
-        formatted_org = Formatter(org).org_div_match()
+        formatted_org = Formatter(org).standardized_org_div()
 
         if formatted_org in formatted_input:
             return org
 
         for div in div_list:
-            formatted_div = Formatter(div).org_div_match()
+            formatted_div = Formatter(div).standardized_org_div()
 
             if formatted_div in formatted_input:
                 return org
@@ -134,7 +158,7 @@ def update_serial_numbers():
         xl_ind_val: int = int(sheet[Tracker.ind_coord].value[-3:])
         xl_grp_val: int = int(sheet[Tracker.grp_coord].value[-3:])
 
-        with open(PathManager.serial_path, "r", encoding="utf-8") as file:
+        with open(path_manager.serial_path, "r", encoding="utf-8") as file:
             data = yaml.safe_load(file)
             yaml_ind_val = data["IND"]
             yaml_grp_val = data["GRP"]
@@ -142,7 +166,7 @@ def update_serial_numbers():
         yaml_ind_val = xl_ind_val if xl_ind_val > yaml_ind_val else yaml_ind_val
         yaml_grp_val = xl_grp_val if xl_grp_val > yaml_grp_val else yaml_grp_val
 
-        with open(PathManager.serial_path, "w", encoding="utf-8") as file:
+        with open(path_manager.serial_path, "w", encoding="utf-8") as file:
             yaml.safe_dump(data, file, indent=4, sort_keys=False, encoding="utf-8")
 
             print(
@@ -162,10 +186,10 @@ def update_serial_numbers():
 class ManualEntry:
     @staticmethod
     def load() -> dict[str, str]:
-        with open(PathManager.manual_entry_path, "r") as file:
+        with open(path_manager.manual_entry_path, "r") as file:
             try:
                 data: dict = yaml.safe_load(file)
-                return {k: str(v) for k, v in data.items()}
+                return {k: Formatter(str(v)).value() for k, v in data.items()}
             except Exception as e:
                 print(e)
 
@@ -173,17 +197,17 @@ class ManualEntry:
     def reset():
         try:
             keys = ManualEntry.load().keys()
-            with open(PathManager.manual_entry_path, "w") as file:
+            with open(path_manager.manual_entry_path, "w") as file:
                 [file.write(f"{k}:\n") for k in keys]
-            print(f"{PathManager.manual_entry_path.name} reset.")
+            print(f"{path_manager.manual_entry_path.name} reset.")
         except Exception as e:
             print(e)
 
 
-def _clean_JSON_output():
+def clean_JSON_output():
     import json
 
-    with open(PathManager.json_output_path, "r", encoding="utf-8") as jfile:
+    with open(path_manager.json_output_path, "r", encoding="utf-8") as jfile:
         data_list: list[dict[str, str]] = json.load(jfile)
         initial_count: int = len(data_list)
 
@@ -196,15 +220,10 @@ def _clean_JSON_output():
 
     items_removed: int = initial_count - final_count
 
-    with open(PathManager.json_output_path, "w", encoding="utf-8") as jfile:
+    with open(path_manager.json_output_path, "w", encoding="utf-8") as jfile:
         json.dump(stripped_list, jfile, indent=4, sort_keys=False)
         print(
-            f"initial count: {initial_count}\n"
+            f"\ninitial count: {initial_count}\n"
             f"items removed: {items_removed}\n"
             f"final count: {final_count}"
         )
-
-
-if __name__ == "__main__":
-    _clean_JSON_output()
-    ManualEntry.reset()
